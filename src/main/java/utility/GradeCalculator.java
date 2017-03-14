5cb0a63b5d180ca861b8c2b787265ed9f586eaf6
AzureAD\PANITATIPPACHAROENSU <panita_tip@cmu.ac.th>
2017-03-14 14:20:32 +0700

    finish calculating a b c x

--- src/main/java/utility/GradeCalculator.java
+++ src/main/java/utility/GradeCalculator.java
@@ -5,7 +5,19 @@ package utility;
  */
 public class GradeCalculator {
     public char calculate(double... scores){
-        return '\0';
+    double sum=0;
+        for (int i=0;i< scores.length; i++) {
+            sum = sum + scores[i];
+        }
+        if(sum<=100&&sum>=0) {
+            if (sum > 95) return 'A';
+            if (sum > 80) return 'B';
+            if (sum > 60) return 'C';
+            return 'F';
+        }else {
+            return 'x';
+        }
+
     }
 
 }
